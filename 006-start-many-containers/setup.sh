c5d9caa3dc7431257310d044f3d255365d0a4add
Ho-Sheng Hsiao <talktohosh@gmail.com>
2015-04-11 22:22:07 -0700

    [006] Cache built copies of ipfs binary

--- 006-start-many-containers/setup.sh
+++ 006-start-many-containers/setup.sh
@@ -5,10 +5,21 @@ export IPFS_PATH=/var/lib/ipfs
 mkdir -p ${GOPATH}
 
 IPFS_BIN="/opt/ipfs/bin/ipfs"
+CACHE_PATH="/vagrant/.cache/"
+CACHED_IPFS_BIN="${CACHE_PATH}/ipfs"
 
 # Compile IPFS from master
-echo "Compiling ipfs from github.com/ipfs/go-ipfs/cmd/ipfs"
-go get -u github.com/ipfs/go-ipfs/cmd/ipfs
+if [ -f ${CACHE_PATH}/ipfs ]; then
+  echo "Cached copy of IPFS binary found. Using cached copy."
+  mkdir -p /opt/ipfs/bin
+  cp ${CACHED_IPFS_BIN} ${IPFS_BIN}
+else
+  echo "Cached copy of IPFS binary not found."
+  echo "Compiling ipfs from github.com/ipfs/go-ipfs/cmd/ipfs"
+  go get -u github.com/ipfs/go-ipfs/cmd/ipfs
+  mkdir -p ${CACHE_PATH}
+  cp ${IPFS_BIN} ${CACHED_IPFS_BIN}
+fi
 
 mkdir -p ${IPFS_PATH}
 
@@ -20,5 +31,6 @@ fi
 ${IPFS_BIN} config Mounts.FuseAllowOther --bool true < /dev/null
 
 cp /home/vagrant/ipfs.conf /etc/init/ipfs.conf
+echo "Starting IPFS"
 service ipfs start
 sleep 10
