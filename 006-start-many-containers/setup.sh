ed784a01be308996f3704260ef3ea083d894a383
Ho-Sheng Hsiao <talktohosh@gmail.com>
2015-04-11 18:20:11 -0700

    [006] Vagrant node will bring up IPFS

---
+++ 006-start-many-containers/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+export GOPATH=/opt/ipfs
+mkdir -p ${GOPATH}
+
+IPFS_BIN="/opt/ipfs/bin/ipfs"
+
+# Compile IPFS from master
+echo "Compiling ipfs from github.com/ipfs/go-ipfs/cmd/ipfs"
+go get -u github.com/ipfs/go-ipfs/cmd/ipfs
+
+/home/vagrant/start-ipfs.sh
