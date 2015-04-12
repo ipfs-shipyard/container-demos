734a1c42666ebb72cf4ab42cc9581ed2485d278b
Ho-Sheng Hsiao <talktohosh@gmail.com>
2015-04-11 22:13:14 -0700

    [006] Fixed bugs, got docker/ipfs working reliably

--- 006-start-many-containers/mount-registry.sh
+++ 006-start-many-containers/mount-registry.sh
@@ -5,7 +5,7 @@ if [ -z ${MASTER_ID} ]; then
   MASTER_ID='local'
 fi
 
-MOUNTPOINT="/ipfs/${MASTER_ID}/docker-registry"
+MOUNTPOINT="/ipns/${MASTER_ID}/docker-registry"
 
 echo "Starting local docker registry mounted on IPFS:"
 echo ${MOUNTPOINT}
@@ -15,6 +15,8 @@ docker run -p 5000:5000 \
            -d -t \
            --name registry \
            -v /etc/docker-registry.yml:/docker-registry.yml \
+           -v ${MOUNTPOINT}:${MOUNTPOINT} \
            -e DOCKER_REGISTRY_CONFIG=/docker-registry.yml \
+           -e SETTINGS_FLAVOR=prod \
            -e STORAGE_PATH=${MOUNTPOINT} \
            registry
--- 006-start-many-containers/setup.sh
+++ 006-start-many-containers/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 export GOPATH=/opt/ipfs
+export IPFS_PATH=/var/lib/ipfs
 mkdir -p ${GOPATH}
 
 IPFS_BIN="/opt/ipfs/bin/ipfs"
@@ -9,6 +10,15 @@ IPFS_BIN="/opt/ipfs/bin/ipfs"
 echo "Compiling ipfs from github.com/ipfs/go-ipfs/cmd/ipfs"
 go get -u github.com/ipfs/go-ipfs/cmd/ipfs
 
-/home/vagrant/start-ipfs.sh
+mkdir -p ${IPFS_PATH}
 
+if [ ! -f ${IPFS_PATH}/config ]; then
+         # < /dev/null needed to get this running under vagrant
+	 ${IPFS_BIN} init < /dev/null
+fi
 
+${IPFS_BIN} config Mounts.FuseAllowOther --bool true < /dev/null
+
+cp /home/vagrant/ipfs.conf /etc/init/ipfs.conf
+service ipfs start
+sleep 10
